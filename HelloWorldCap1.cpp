82c183be5e8c1b491d3bc307c453825d59354282
Jorge28533 <39318417+Jorge28533@users.noreply.github.com>
2018-09-06 17:50:09 -0500

    Capitulo 1 C++
    Aqui esta las actividades del capitulo 2.

---
+++ HelloWorldCap1.cpp
@@ -0,0 +1,14 @@
+#include "Includes.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hola Mundo, Soy Jorge!!\n";  //Salida de Hola mundo
+	keep_window_open(); // espera a que se ingrese un caracter
+	return 0;
+
+
+
+}
