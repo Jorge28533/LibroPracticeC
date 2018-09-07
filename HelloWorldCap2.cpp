e9eb35c3f34ab53fe15f820383b23c6cd37fcda9
Jorge28533 <39318417+Jorge28533@users.noreply.github.com>
2018-09-07 18:44:54 -0500

    Capitulo 2 ejercicios
    Aqui esta la primera tarea, y una rica receta de pay de queso.

---
+++ HelloWorldCap2.cpp
@@ -0,0 +1,123 @@
+#include "Includes.h"
+#include <iostream>
+
+using namespace std;
+
+
+/*
+Esperas un valor de retorno de tipo entero
+int main()
+{
+return 0
+}
+valor de retorno es nulo
+void funcion(....)
+{
+}
+Una funcion de suma
+int Suma(int x, int y)
+{
+return x + y
+}
+1-para paramtros que no sean por defaul hasta la derecha
+
+
+
+
+*/
+int main()
+{
+	//Ejercicio 1
+	
+	cout << "Hola,programador!!\n"; //Salida de Hola mundo
+	cout << "Aqui Vamos a programar!!\n"; //salida de Aqui programando
+	cout << "Wiii!!!\n"; // salida de emocionado a programar wiii!!!
+	cout << "\n";
+	// Ejercicio 2	
+	cout << "1.Levantate, 2.Gira a la derecha\n";
+	cout << "3.Gira otra vez a la derecha 4.Levanta una pierna\n";
+	cout << "5.mueve hacia delante tu pie y avanza, 6.Repite pasos 4 y 5, 6 veces\n";
+	cout << "7.1 pie levantalo y pon arriba del primer escalon 8.Reune el otro pie con el otro\n";
+	cout << " 9. Si hay pared enfrente gire derecha hasta que npo haya 10.Repite 7 y 8 hasta que no haya escalones\n";
+	cout << "11.Avanzar 12.Al llegar al baño abrir la puerta 13.avanzar adentro del baño\n";
+	cout << "14.ponerte de espaldas del retrete 15.sentarte encima del retrete en donde esta compuerta de salida\n";
+	cout << "16.abrir compuerta de salida 17.sacar todos los desechos 18.Terminaste de hacer del baño\n";
+	cout << "\n";
+	//Ejercicio 3
+	cout << "1.Agarrar 14 pesos 2.Agarrar la llave de la puerta\n";
+	cout << "3.dirigirce a la puerta de salida de la casa 4.abrir la puerta con la llave\n";
+	cout << "5.Jalar la manija y jalar la puerta 6.atravezar la puerta\n";
+	cout << "7.Cerrar la puerta y cerrarla con llave 8.Caminar hacia la derecha hasta la primera Avenida\n";
+	cout << "9.Girar a la derecha 10.ir todo derecho hasta las vias de tren\n";
+	cout << "11.girar a la derecha 12.avanzar hasta la avenida 13.girar a la izquierda\n";
+	cout << "14.avanzar hasta la avenida mexico todo derecho\n";
+	cout << "15.ponerte en la parada del camion 16.Pedir el camion cuando pase el camion 29 o 39A\n";
+	cout << "17.Sacar 7 pesos 18.Subir el camion y dar los 7 pesos 19.Esperar hasta que llegue a la parada de galerias\n";
+	cout << "20.pedir parada 21.parado el camion bajar 22.caminar a la parada del 27\n";
+	cout << "23.Esperar el 27 24.pedir parada al 27 cuando aparezca 25.Sacar los 7 pesos que faltan\n";
+	cout << "26.Subir al autobus y dar los 7 pesos 27.Buscar asiento 28.Sentarce en espacio libre\n";
+	cout << "29.sacar libro 30.leer hasta antes de llegar a escuela 31.guardar libro\n";
+	cout << "32.Pararce a la puerta 33.pedir parada antes de llegar a la escuela\n";
+	cout << "34.Bajar cuando este parado el autobus 35.dar media vuelta\n";
+	cout << "36.atravezar la calle 37.timbrar la puerta de la escuela 38.Abrir la puerta\n";
+	cout << "39.Entrar a la escuela 40.Cerrar la puerta y felicidades llegaste a la escuela\n";
+	cout << "\n";
+	//Ejercicio 4
+	cout << "Pay de queso\n";
+	cout << "Ingredientes: \n";
+	cout << "1.Galletas Maria\n";
+	cout << "2.Molde de Pay de queso\n";
+	cout << "3.1 lata de Lechera\n";
+	cout << "4.Mantequilla\n";
+	cout << "5.Leche endulzada\n";
+	cout << "6.Queso Crema\n";
+	cout << "Materiales:\n";
+	cout << "Licuadora\n";
+	cout << "Regrijerador\n";
+	cout << "Chuchara\n";
+	cout << "Recipiente Ondo\n";
+	cout << "Estufa\n";
+	cout << "Casuela\n";
+	cout << "Intrucciones: \n";
+	cout << "1.Poner varias galletas en la licuadora y licuar las galletas \n";
+	cout << "2.Poner la mantequilla en la casuela\n";
+	cout << "3.Poner la casuela en la estufa y prender a medio fuego\n";
+	cout << "Pay de queso\n";
+	cout << "4.Esperar a que se derrita la mantequilla totalmente y apagar cuando pase\n";
+	cout << "5.Ya licuadas las galletas vaciar en un recipiente ondo\n";
+	cout << "6.Vertir la mantequilla derretida en el plato ondo\n";
+	cout << "7.Con una chuchara revolver la mantequilla y la galleta molida\n";
+	cout << "8.Lavarce las manos\n";
+	cout << "9.Ya revuelto agarras con la mano la galleta y la vas pegando alrededor del molde de pay sin dejar un bueko\n";
+	cout << "10.Echar el queso crema en la licuadora\n";
+	cout << "11.Agarrar la leche endulzada y ponerla en la licuadora\n";
+	cout << "12.Poner el endulzante de Limon\n";
+	cout << "13.Licuar las tres cosas hasta que este espeso\n";
+	cout << "14.Vertir la mezcla en el recipiente de pay ya con la galleta bien puesta  \n";
+	cout << "15.Agarrar el recipiente de pay y meterlo en el congelador durante aproximadamente Durabte 12 horas \n";
+	cout << "16.Listo para comer!! Wiii!! \n";
+	cout << "\n";
+	// Ejercicio 5
+	cout << "//: es para poder comentar sin elo copilador lo lea,es mas para uso humano para saber lo que hace un pedazo de codigo\n";
+	cout << "<<:es para darle una dirrecion al lo que se quiere haser \n";
+	cout << "C++: Es un lenguage de programacion enfocado al uso de objetos\n";
+	cout << "Compiler: es la recopilacion del codigo\n";
+	cout << "Compiler-time-error:Es un error a la hora de recopilar\n";
+	cout << "cout:se dice see-out es para mandar lo que esta en el a la pantalla\n";
+	cout << "funtion:Es un pedazo de codigo que hace algo\n";
+	cout << "Executable:\n";
+	cout << "header:\n";
+	cout << "IDE:\n";
+	cout << "#Include:es para incluir una libreria o un documento h\n";
+	cout << "Library:Es un archivo donde se encuentran las funciones exclusivas de C++\n";
+	cout << "Linker:\n";
+	cout << "main():es la funcion donde c++ detecta donde empezar.\n";
+	cout << "object code:\n";
+	cout << "output: es un dato de salida que es el resultado de una operacion\n";
+	cout << "program:\n";
+	cout << "source code:\n";
+	cout << "statement:\n";
+
+	keep_window_open(); // espera a que se ingrese un caracter
+	return 0;
+}
