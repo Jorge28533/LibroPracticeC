342681f5e0c94e9c1ff928df5fe24ce7f34835e0
Jorge28533 <39318417+Jorge28533@users.noreply.github.com>
2018-09-11 17:58:47 -0500

    Capitulo 3
    Aqui estan los ejercicios del capitulo 3 del libro de C++

---
+++ Capitulo3.cpp
@@ -0,0 +1,316 @@
+#include "Includes.h"
+
+int main()
+{
+	/* Drill
+	cout << "Ingrese el nombre de la persona que le desee escribir:\n";
+	string first_name; // first_name is a variable of type string
+	cin >> first_name; // read characters into first_name
+	cout << "Estimado, " << first_name << "¿Como esta?\n";
+	cout << "Lo extrañamos por haca\n";
+	cout << "otro de amigo\n";
+	string amigo_name;
+	cin >> amigo_name;
+	cout << "De casualida has visto a " << amigo_name << " Ultimamente?\n";
+	cout << "De que sexo es tu amigo Masculino? = m Femenino = f\n";
+	char sex_amigo = '0';
+	cin >> sex_amigo;
+	sex_amigo = sex_amigo;
+	if (sex_amigo == 'm')
+	cout << "si ves " << amigo_name << "pidele por favor que me llame\n";
+	if (sex_amigo == 'f')
+	cout << "si ves " << amigo_name << "preguntale si me puede llamar\n";
+	cout << "Cual es la edad del destinatario?";
+	int edad = 0;
+	cin >> edad;
+	edad = edad;
+	if (edad == 0 || edad > 110)
+	cout << "Estas bromeando?\n";
+	if (edad == 17)
+	cout << "En un año podras votar en las elecciones\n";
+	if (edad >= 70)
+	cout << "Espero que estes disfrutando de tu retiro\n";
+	cout << "Atentamente\n";
+	cout << "\n";
+	cout << "Jorge Eduardo Fonseca\n";
+	keep_window_open();
+	*/
+
+
+	//Ejercicio 2
+	/*
+	cout << "Ingrese el numero de millas que quieras convertir en kilometros!!\n";
+	double milla = 0;
+	cin >> milla;
+	milla = milla;
+	milla *= 1.609;
+	cout << "Las millas convertidas a kilometros son: " << milla << "Kilometros\n";
+	keep_window_open();
+	*/
+
+
+	//Ejercicio 3
+	/*
+	int hola = 0.2;
+	int double = 4.5;
+	double k = 'm';
+	//Muere la pobre caomputadora y no quiere hacerlo.
+	*/
+
+	
+	//Ejercicio 4
+	/*
+	cout << "Ingrese un numero Entero: \n";
+	int  val1 = 0;
+	cin >> val1;
+	val1 = val1;
+	cout << "Ingrese otro numero Entero: \n";
+	int  val2 = 0;
+	cin >> val2;
+	val2 = val2;
+	cout << "La Suma de los dos valores es: " << val1 + val2 << "\n";
+	cout << "La Resta de los dos valores es: " << val1 - val2 << "\n";
+	cout << "La Multiplicacion de los dos valores es: " << val1 * val2 << "\n";
+	cout << "La Division de los dos valores es: " << val1 / val2 << "\n";
+	cout << "La Modulo de los dos valores es: " << val1 % val2 << "\n";
+	keep_window_open();
+	*/
+
+	//Ejercicio 5
+	/*
+	cout << "Ingrese un numero doble: \n";
+	double  val1 = 0;
+	cin >> val1;
+	val1 = val1;
+	cout << "Ingrese otro numero doble: \n";
+	double  val2 = 0;
+	cin >> val2;
+	val2 = val2;
+	cout << "La Suma de los dos valores es: " << val1 + val2 << "\n";
+	cout << "La Resta de los dos valores es: " << val1 - val2 << "\n";
+	cout << "La Multiplicacion de los dos valores es: " << val1 * val2 << "\n";
+	cout << "La Division de los dos valores es: " << val1 / val2 << "\n";
+	cout << "no puede sacar el modulo\n";
+	keep_window_open();
+	*/
+
+	
+	//Ejercicio 6
+	/*
+	cout << "Ingrese un numero Entero val1: \n";
+	int  val1 = 0;
+	cin >> val1;
+	val1 = val1;
+	cout << "Ingrese otro numero Entero val2: \n";
+	int  val2 = 0;
+	cin >> val2;
+	val2 = val2;
+	cout << "Ingrese el tercer numero Entero val3: \n";
+	int  val3 = 0;
+	cin >> val3;
+	val3 = val3;
+	if (val3 >= val2 && val3 >= val1)
+	{
+		if (val2 >= val1)
+		{
+			cout << val1 << ", " << val2 << ", " << val3 << "\n";
+		}
+		if (val2 <= val1)
+		{
+			cout << val2 << ", " << val1 << ", " << val3 << "\n";
+		}
+	}
+	if (val2 >= val1 && val2 >= val3)
+	{
+		if (val3 >= val1)
+		{
+			cout << val1 << ", " << val3 << ", " << val2 << "\n";
+		}
+		if (val3 <= val1)
+		{
+			cout << val3 << ", " << val1 << ", " << val2 << "\n";
+		}
+	}
+	if (val1 >= val2 && val1 >= val3)
+	{
+		if (val2 >= val3)
+		{
+			cout << val3 << ", " << val2 << ", " << val1 << "\n";
+		}
+		if (val2 <= val3)
+		{
+			cout << val2 << ", " << val3 << ", " << val1 << "\n";
+		}
+	}
+	keep_window_open();
+	*/
+
+	
+	//Ejercicio 7
+	/*
+cout << "Ingrese una palabra val1: \n";
+string  val1;
+cin >> val1;
+cout << "Ingrese otro palabra val2: \n";
+string  val2 ;
+cin >> val2;
+cout << "Ingrese el tercer palabra val3: \n";
+string  val3;
+cin >> val3;
+if (val3 >= val2 && val3 >= val1)
+{
+	if (val2 >= val1)
+	{
+		cout << val1 << ", " << val2 << ", " << val3 << "\n";
+	}
+	if (val2 <= val1)
+	{
+		cout << val2 << ", " << val1 << ", " << val3 << "\n";
+	}
+}
+if (val2 >= val1 && val2 >= val3)
+{
+	if (val3 >= val1)
+	{
+		cout << val1 << ", " << val3 << ", " << val2 << "\n";
+	}
+	if (val3 <= val1)
+	{
+		cout << val3 << ", " << val1 << ", " << val2 << "\n";
+	}
+}
+if (val1 >= val2 && val1 >= val3)
+{
+	if (val2 >= val3)
+	{
+		cout << val3 << ", " << val2 << ", " << val1 << "\n";
+	}
+	if (val2 <= val3)
+	{
+		cout << val2 << ", " << val3 << ", " << val1 << "\n";
+	}
+}
+keep_window_open();
+	*/
+	
+	
+	//Ejercicios 8
+	
+	/*
+	cout << "Que numero deceas saber si es par o no par?: ";
+	int val = 0;
+	cin >> val;
+	val = val;
+	if (val % 2 == 0)
+	{
+		cout << "Este numero es par\n";
+	}
+	if (val % 2 == 1)
+	{
+		cout << "Este numero es impar\n";
+	}
+	keep_window_open();
+	*/
+
+	
+	//Ejercicio 9
+	/*
+	cout << "Escribe un numero del 1 al 4 en palabras o numero: ";	
+	string palabra;
+	string uno;
+	string dos;
+	string tres;
+	string cuatro;
+	string cero;
+	cin >> palabra;
+	if(palabra == "cero")
+	{
+		cout << "0\n"; 
+	}
+	if (palabra == "uno")
+	{
+		cout << "1\n";
+	}
+	if (palabra == "dos")
+	{
+		cout << "2\n";
+	}
+	if (palabra == "tres")
+	{
+		cout << "3\n";
+	}
+	if (palabra == "cuatro")
+	{
+		cout << "4\n";
+	}
+	if (palabra == "4")
+	{
+		cout << "cuatro\n";
+	}
+	if (palabra == "3")
+	{
+		cout << "tres\n";
+	}
+	if (palabra == "2")
+	{
+		cout << "dos\n";
+	}
+	if (palabra == "1")
+	{
+		cout << "uno\n";
+	}
+	if (palabra == "0")
+	{
+		cout << "cero\n";
+	}
+	keep_window_open();
+	*/
+
+	
+	//Ejercicio 10
+	/*
+	string operando;
+	double val1;
+	double val2;
+	cin >> operando; cin >> val1; cin >> val2;
+	if (operando == "+")
+		cout << "Es una Suma: " <<val1 + val2 << "\n";
+	if (operando == "-")
+		cout << "Es una Resta: " << val1 - val2 << "\n";
+	if (operando == "*")
+		cout << "Es una Multiplicaion: " << val1 * val2 << "\n";
+	if (operando == "/")
+		cout << "Es una Division: " << val1 / val2 << "\n";
+	keep_window_open();
+	*/
+
+	//Ejercicio 11
+	double centavos = 0; // 1 centavo
+	double nikels = 0; // 5 centavos
+	double dimes = 0; // 10 centavos
+	double quarks = 0; // 25 centavoa
+	double half_dollars = 0; // 50 centavos
+	double one_dollar = 0; // 100 centavos
+	cout << "Cuantos centavos tiene?\n ";
+	cin >> centavos;
+	cout << "Cuantos nikels tiene?\n ";
+	cin >> nikels;
+	cout << "Cuantos dimes tiene?\n ";
+	cin >> dimes;
+	cout << "Cuantos quarks tiene?\n ";
+	cin >> quarks;
+	cout << "Cuantos half_dollar tiene?\n ";
+	cin >> half_dollars;
+	cout << "Cuantos one_dollar tiene?\n ";
+	cin >> one_dollar;
+
+	nikels *= 5;
+	dimes *= 10;
+	quarks *= 25;
+	half_dollars *= 50;
+	one_dollar *= 100;
+
+	cout << "$" << centavos + nikels + dimes + quarks + half_dollars + one_dollar / 100.0 << "Dolares.\n";
+	keep_window_open();
+
+}
